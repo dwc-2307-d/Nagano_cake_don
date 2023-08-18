f231911c5abe5f0d45ab94760a410a80b365bac9
hideo333666 <137790586+hideo333666@users.noreply.github.com>
2023-08-18 09:09:17 +0000

    ジャンルコントローラー作成、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,21 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   devise_for :users
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+  #public用のルーティング
+  scope module: :pubic do
+  end
+  
+  
+  
+  
+  
+  
+  
+  #管理者側のルーティング設定
+  namespace :admin do
+  end
