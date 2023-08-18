7c41c728f9377853f70bec6ad0bad1d9660fb91a
RyosukeSuzuki <138420782+Ryosuke2715@users.noreply.github.com>
2023-08-18 09:25:59 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
 
   #public用のルーティング
   scope module: :pubic do
@@ -20,3 +20,4 @@ end
   #管理者側のルーティング設定
   namespace :admin do
   end
+end
