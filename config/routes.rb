bb9469662854ca4f23f096cdbbcc47c09b44a052
defo-wd <default.9110@gmail.com>
2023-08-18 13:35:06 +0000

    Header,Footerの仮コミット

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,22 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   devise_for :users
-  
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
   #public用のルーティング
-  scope module: :pubic do
+  scope module: :public do
+    root to: "homes#top"
+    get "customers/about"=>"homes/about"
   end
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
   #管理者側のルーティング設定
   namespace :admin do
   end
