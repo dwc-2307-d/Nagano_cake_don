e82a537207d9068fbeac81ecd4734f359b093de3
hideo333666 <muramura031409@gmail.com>
2023-08-19 08:08:37 +0000

    商品新規登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,21 +12,18 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/feature/homes_layouts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
   #public用のルーティング
   scope module: :public do
     root to: "homes#top"
-<<<<<<< HEAD
+
     get "about"=>"homes#about"
-=======
+
     get "customers/about"=>"homes/about"
->>>>>>> origin/feature/homes_layouts
+
   end
 
 
@@ -37,6 +34,9 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   #管理者側のルーティング設定
   namespace :admin do
+    resources :items,except: [:destroy]
     resources :genres,except: [:new,:destroy]
+    
+    
   end
 end
