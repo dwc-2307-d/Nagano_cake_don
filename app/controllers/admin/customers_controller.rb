de0b387fe02dbef09a4ecf73d6760a8ed28b7f21
ucircle <137670323+ucircle@users.noreply.github.com>
2023-08-20 15:25:06 +0000

    customersindex

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,31 @@
+class Admin::CustomersController < ApplicationController
+
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "登録情報を変更しました。"
+      redirect_to admin_customers_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :post_code, :telephone_number, :is_deleted)
+  end
+end
