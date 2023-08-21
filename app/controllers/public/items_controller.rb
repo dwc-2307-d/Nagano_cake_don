f7d412c201bad60c845f6d1206dd6db9d042de44
hideo333666 <muramura031409@gmail.com>
2023-08-21 05:29:30 +0000

    商品一覧修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
   
   def index 
-    @item = Item.page(params[:page]).per(5)
+    @items = Item.page(params[:page]).per(5)
     @items_all = Item.all
   end
   
