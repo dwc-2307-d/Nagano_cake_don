38b5e3de2102769cc04621d99d8e2d7d6889f8fd
defo-wd <default.9110@gmail.com>
2023-08-26 06:08:12 +0000

    検索機能の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,11 @@ class Item < ApplicationRecord
   validates :genre_id, presence: true
   validates :is_status, inclusion: { in: [true, false], message: "の選択は必須です" }
 
+  # 検索
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "genre_id", "id", "is_status", "name", "price", "updated_at"]
+  end
+
   # Instance methods
   def item_status
     is_status ? "販売中" : "販売停止中"
