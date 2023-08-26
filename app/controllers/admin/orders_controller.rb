6ae8dd71031ce24068515519cb259fa72fec49b8
ucircle <137670323+ucircle@users.noreply.github.com>
2023-08-26 06:01:22 +0000

    status_update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,10 +12,10 @@ class Admin::OrdersController < ApplicationController
   @order = Order.find(params[:id])
   @order_items = OrderItem.where(order_id: params[:id])
   if @order.update(order_params)
-     if @order.status == "payment_confirmation"
+     if @order.confirm_payment?
     @order_items.update_all(making_status: 1)
     ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
- 　　 end
+     end
   redirect_to admin_order_path(@order)
   end
   end
